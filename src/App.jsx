e85bd55f46f0118b53831f6b91369260a752a139
RAYHAN-HEXA <mohammadrayhan796@gmail.com>
2025-10-25 10:11:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+
+
+
+<h1>Simple Firebase Auth</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+          return (
+                    <div>
+                          <h1>Please LOgin</h1>    
+                    </div>
+          );
+};
+
+export default Login;
